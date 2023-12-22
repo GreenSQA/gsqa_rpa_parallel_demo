d220996c5de648e14968b50747404f6462bfc67e
Eider Mauricio <eidermauricio@gmail.com>
2023-12-22 17:35:22 -0500

    connect to gh cbmplay cal

--- scripts/dev queries.sql
+++ scripts/dev queries.sql
@@ -1,5 +1,10 @@
 select * from dbo.job_target_data_master
 select * from dbo.job_target_data
 
--- DELETE from dbo.job_target_data
--- DELETE from dbo.job_target_data_master
+/*
+
+   DELETE from dbo.job_target_data
+   DELETE from dbo.job_target_data_master
+
+
+*/
